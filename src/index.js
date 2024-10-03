8d09c22edc3d6d7202496551ef9f2299ed083d0a
divyanshucodes20 <155738552+divyanshucodes20@users.noreply.github.com>
2024-10-03 23:05:55 +0530

    utils added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import mongoose from "mongoose"
-import {DB_NAME} from "./constants.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config({
     path:'./env'
 })
 connectDB()
+app.on("error",(error)=>{
+    console.log("Error",error);
+    throw error
+})
+.then(()=>{
+    app.listen(process.env.Port||8000,()=>{
+        console.log(`Server is running on port ${process.env.Port||8000}`);
+    })
+})
+.catch((error)=>{
+    console.log("Mongo db connection failed!!!",error);
+})
 /*
 import express from "express"
+import mongoose from "mongoose"
+import {DB_NAME} from "./constants.js";
 const app=express()
 
 ;(async()=>{
