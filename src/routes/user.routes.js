eb78a8b9fc3ec4dea393b9cb16b736ea52274b37
divyanshucodes20 <155738552+divyanshucodes20@users.noreply.github.com>
2024-10-04 17:35:14 +0530

    fixes some bugs and make controller of userregister

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import {Router} from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 const router =Router()
-router.route("/register").post(registerUser)
+router.route("/register").post(
+upload.fields({
+name:"avatar",
+maxCount:1
+},
+{
+    name:"coverImage",
+    maxCount:1
+}
+),  
+    registerUser
+)
 export default router//jab export default karte hai tab usko import router aise karte hai aur agar isko hum aise karte ki export {router} then humko isko import {router} karna padta
